b756034dfe42f8e0e3c2194b6f3c6de4984abfcf
moneky677 <dojyaaan650@gmail.com>
2026-05-09 20:51:31 -0400

    Refactor Rock Paper Scissor game logic

--- rockpaperscissorgame.py
+++ rockpaperscissorgame.py
@@ -1,78 +1,37 @@
 import random
+op1 = "rock"
+op2 = "paper"
+op3 = "scissor"
+options = [op1,op2,op3]
+botoption = [op1,op2,op3]
+print("Welcome to Rock Paper Scissor Game!")
 def code():
-  print('Welcome to rock paper scissor game.')
-  op1 = 'rock' 
-  op2 = 'paper'
-  op3 = 'scissor'
-  option = [op1,op2,op3]
-  player_option = ["1" , "2" , "3"]
-  choice = int(input("Please select your choice: press '1' for Rock , '2' for Paper , '3' for scissor\nPlease type out your choice correctly: \n"))
-  bot = random.choice(option)
-  def outcome():
-    if choice == op1 and bot == op2 or choice == op2 and bot == op3 or choice == op3 and bot == op1:
-      print("\nLMAO,YOU LOST TO A BOT IN ROCK PAPER SCISSOR!?\n\nHOLY SKILL ISSUE!!!!!!!!\n")
+  while True:
+    bot = random.choice(botoption)
+    option = input("\nPlease enter your choice.\n\n press 1 for rock\n\n press 2 for paper\n\n press 3 for scissor\n\n press 4 to exit...\n\nEnter your choice: \n")
+    if not option.isdigit():
+      print("Not an option :(")
       code()
-    elif choice == op1 and bot == op3 or choice == op2 and bot == op1 or choice == op3 and bot == op2:
-      print("\nGood job!! You win!\n \nYou may now have dinner cuz you didn't fail.\n")
-      code()
+    choice = option
+    choice = int(choice)
+    if choice == 1:
+      choice = op1
+    elif choice == 2:
+      choice = op2
+    elif choice == 3:
+      choice = op3
+    if choice == op1 and bot == op3 or choice == op2 and bot == op1 or choice == op3 and bot == op2:
+        print("\nYOU PICKED: " + choice+"\n")
+        print("\nBOT PICKED: " + bot)
+        print("You win!")
+        code()
+    elif choice == op1 and bot == op2 or choice == op2 and bot == op3 or choice == op3 and bot == op1:
+        print("YOU PICKED: " + choice+"\n")
+        print("\nBOT PICKED: " + bot)
+        print("LMAO, YOU LOST TO A BOT !? LITTERALLY HOW? YOU SUCK AT THE OLDEST GAME IN THE BOOK!!")
+        code()
     elif choice == bot:
-      print("\nTIE!!!\n")
-      code()
-  if choice == 1:
-    choice = op1
-    print("You picked: " + op1)
-    print(" ")
-    print("Computor picked: " + bot)
-  elif choice == 2:
-    choice = op2
-    print("You picked: " + op2)
-    print(" ")
-    print("Computor picked: " + bot)
-  elif choice == 3:
-    choice = op3
-    print("You picked: " + op3)
-    print(" ")
-    print("Computor picked: " + bot)
-  else:
-    print("Not an option :( ")
-    code()
-  outcome()
-  
-import random
-print('Welcome to rock paper scissor game.')
-op1 = 'rock' 
-op2 = 'paper'
-op3 = 'scissor'
-option = [op1,op2,op3]
-player_option = ["1" , "2" , "3"]
-choice = int(input("Please select your choice: press '1' for Rock , '2' for Paper , '3' for scissor\nPlease type out your choice correctly: \n"))
-bot = random.choice(option)
-def outcome():
-  if choice == op1 and bot == op2 or choice == op2 and bot == op3 or choice == op3 and bot == op1:
-    print("\nLMAO,YOU LOST TO A BOT IN ROCK PAPER SCISSOR!?\n\nHOLY SKILL ISSUE!!!!!!!!\n")
-    code()
-  elif choice == op1 and bot == op3 or choice == op2 and bot == op1 or choice == op3 and bot == op2:
-    print("\nGood job!! You win!\n \nYou may now have dinner cuz you didn't fail.\n")
-    code()
-  elif choice == bot:
-    print("\nTIE!!!\n")
-    code()
-if choice == 1:
-    choice = op1
-    print("You picked: " + op1)
-    print(" ")
-    print("Computor picked: " + bot)
-elif choice == 2:
-  choice = op2
-  print("You picked: " + op2)
-  print(" ")
-  print("Computor picked: " + bot)
-elif choice == 3:
-  choice = op3
-  print("You picked: " + op3)
-  print(" ")
-  print("Computor picked: " + bot)
-else:
-  print("Not an option :( ")
-  code()
-outcome()
+      print("YOU PICKED: " + choice+"\n")
+      print("BOT PICKED: " + bot)
+      print("TIE!!!")
+code()
